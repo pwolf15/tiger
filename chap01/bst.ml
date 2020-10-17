0f01b60a34ec55db23eb5331b179b93d41a08c73
pwolf15 <patrick.wolf@live.com>
2020-10-16 21:46:09 -0700

    Reverted insert

--- chap01/bst.ml
+++ chap01/bst.ml
@@ -9,9 +9,9 @@ fun insert(key, LEAF) = TREE(LEAF, key, LEAF)
   | insert(key, TREE(l, k, r)) =
       if key < k
         then TREE(insert(key,l),k,r)
-      else if key >= k
+      else if key > k
         then TREE(l, k, insert(key, r))
-      else TREE(l, k, insert(key, r))
+      else TREE(l, k, r)
 
 fun member(key, LEAF) = false
   | member(key, TREE(l, k, r)) =
